5b9468727155cf5c6677618ee2ece4a466b66f06
fizzer <fizzer@enph353.JoDo>
2019-11-10 19:02:19 -0800

    commit before experimenting with the length of lines

--- src/robotController.py
+++ src/robotController.py
@@ -24,8 +24,10 @@ class robot_controller:
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
         self.velocity_cmd = rospy.Publisher('/R1/cmd_vel', Twist,queue_size=1)
+        self.prevSlope = 0
 
     def callback(self,data):
+        infinityThresh = 1.4
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
@@ -41,17 +43,18 @@ class robot_controller:
         M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
         Minv = cv2.getPerspectiveTransform(dst, src) # Inverse transformation
 
-        img = cv_image
-        img = img[450:(450+IMAGE_H), 0:IMAGE_W] # Apply np slicing for ROI crop
-        warped_img = cv2.warpPerspective(img, M, (IMAGE_W, IMAGE_H)) # Image warping
-
+    
+        cv_image = cv_image[450:(450+IMAGE_H), 0:IMAGE_W] # Apply np slicing for ROI crop
+        warped_img = cv2.warpPerspective(cv_image, M, (IMAGE_W, IMAGE_H)) # Image warping
 
         #color masks 
         #detecting lines on the street
-        lowerWhite = np.array([0, 200, 0],dtype = "uint8")
+        lowerWhite = np.array([250, 250, 250],dtype = "uint8")
         upperWhite = np.array([255, 255, 255],dtype = "uint8")
         whiteMask = cv2.inRange(warped_img, lowerWhite, upperWhite)
-	whiteMask = cv2.erode(whiteMask, None, iterations=2)
+        # whiteMask = cv2.blur(whiteMask, (5,5))
+        # whiteMask = cv2.medianBlur(whiteMask, 19)
+        whiteMask = cv2.erode(whiteMask, None, iterations=2)
 	# whiteMask = cv2.dilate(whiteMask, None, iterations=2)
         #detecting the street
         lowerGray = np.array([0, 0, 50],dtype = "uint8")
@@ -78,39 +81,52 @@ class robot_controller:
         
         edges = cv2.Canny(whiteOutput, 100, 200)
         lines = cv2.HoughLinesP(edges,1,np.pi/180, 20, 100)
-        m = []
+        m = [] #list for slopes of the detected lines 
 
+        STRAIGHT = 4000
         if (lines is None):
-            self.pid(0)
+            self.pid(STRAIGHT) #go straight
         else:
             for line in lines:
                 for points in line:
-                    # print(points)
-                    m.append(float (points[0] - points[2]) / (points[1] - points[3]))
+                    m.append(float (points[1] - points[3]) / (points[0] - points[2])) #  m = y/x
             m = [x for x in m if str(x) != 'nan'] #gets rid of nan values
             sumOfSlopes = 0
             numOfSlopes = 0
             numberOfInfinity = 0
+            # print(m)
             for slope in m:
                 if slope == float("-inf") or slope == float("inf"):
                     numberOfInfinity = numberOfInfinity + 1
                 else:
                     sumOfSlopes = sumOfSlopes + slope
-                    averageSlope = sumOfSlopes / numOfSlopes
                     numOfSlopes = numOfSlopes + 1
             # print("slopes")
             # print (m)
             # print ("sum of slopes")
             # print (sumOfSlopes)
-            # print("num of slopes")
-            # print(numOfSlopes)
-            # print("num of inf slopes")
-            # print(numberOfInfinity)
+            print("num of slopes")
+            print(numOfSlopes)
+            print("num of inf slopes")
+            print(numberOfInfinity)
+            if(numberOfInfinity > infinityThresh*numOfSlopes): #we should go straight
+                averageSlope = float("inf")
+            else: 
+                averageSlope = sumOfSlopes / numOfSlopes #we should turn
             print("average slope")
             print(averageSlope)
+            longestLength = 0
+            longestLine = [0,0,0,0]
+            #drawing the hough lines on the white mask
+            cv2.line(whiteOutput,(int(cols/2),int(rows/2)), (int(20 / averageSlope + cols/2), int(rows/2 + 20)),(255,0,0),2)
             for i in range(len(lines)):
                 for x1,y1,x2,y2 in lines[i]:
-                    cv2.line(whiteOutput,(x1,y1), (x2,y2), (0,255,0), 2)
+                    length = ((x2-x1)**2 + (y2-y1)**2)**(0.5)
+                    if(length > longestLength):
+                        longestLength = length
+                        longestLine = [x1,y1,x2,y2]
+                    # cv2.line(whiteOutput,(x1,y1), (x2,y2), (0,255,0), 2)
+            cv2.line(whiteOutput,(longestLine[0],longestLine[1]),(longestLine[2],longestLine[3]), (0,0,255), 2)
             # cv2.imwrite("hough lines.jpg",warped_img)
 
             # rowW,colW,rgbW = np.nonzero(whiteOutput)
@@ -141,16 +157,6 @@ class robot_controller:
             # cv2.circle(warped_img,(targetLocation,20),10,255)
             # cv2.circle(warped_img,(mostFrequentColumnGY,20),10,0)
             # cv2.circle(warped_img,(mostFrequentColumnW,20),10,120)
-            # height,width,channels = warped_img.shape
-            # middlePixel = width / 2
-            # cv2.circle(warped_img,(middlePixel,20),10,200)
-            # print("middle pixel")
-            # print(middlePixel)
-
-            # if(middlePixel > targetLocation):
-            #     print("turning left")
-            # else:
-            #     print("turning right")
 
             cv2.imshow("whiteMask",whiteOutput)
             cv2.waitKey(3)
@@ -160,20 +166,20 @@ class robot_controller:
             self.pid(averageSlope)
 
     def pid(self,slope):
-        slopeThresh = 0.05
-        angularScale = 5
+        slopeThresh = 5000
+        angularScale = 2
         xVelocity = 0.03
         zTwist = 0.0
-        if(abs(slope) > slopeThresh ):
+        if(abs(slope) < slopeThresh and abs(self.prevSlope) > slopeThresh):
             xVelocity = 0.0 
-            zTwist = angularScale * slope
+            zTwist = float(angularScale  / slope)
+            self.prevSlope = slope 
         else:
-            zTwist = 0.0
-        #xDifference>0 -> line on right
+            self.prevSlope = float("inf")
         vel_msg = Twist()
         vel_msg.linear.x = xVelocity
         vel_msg.angular.z = zTwist
-        print (vel_msg)
+        # print (vel_msg)
         self.velocity_cmd.publish(vel_msg)
         
 
