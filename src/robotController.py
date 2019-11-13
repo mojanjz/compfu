eddd2c9273738d2949ffaf8b24e7e59961145f7b
fizzer <fizzer@enph353.JoDo>
2019-11-12 18:22:34 -0800

    garbage code

--- src/robotController.py
+++ src/robotController.py
@@ -27,7 +27,7 @@ class robot_controller:
         self.prevSlope = 0
 
     def callback(self,data):
-        infinityThresh = 1.4
+        infinityThresh = 1.5
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
@@ -36,7 +36,6 @@ class robot_controller:
 
         IMAGE_H = rows
         IMAGE_W = cols
-
         #birdeye view of the image
         src = np.float32([[0, IMAGE_H], [1207, IMAGE_H], [0, 0], [IMAGE_W, 0]])
         dst = np.float32([[569, IMAGE_H], [711, IMAGE_H], [0, 0], [IMAGE_W, 0]])
@@ -52,8 +51,8 @@ class robot_controller:
         lowerWhite = np.array([250, 250, 250],dtype = "uint8")
         upperWhite = np.array([255, 255, 255],dtype = "uint8")
         whiteMask = cv2.inRange(warped_img, lowerWhite, upperWhite)
-        # whiteMask = cv2.blur(whiteMask, (5,5))
-        # whiteMask = cv2.medianBlur(whiteMask, 19)
+        # whiteMask = cv2.GaussianBlur(whiteMask, (5,5), 0)
+        whiteMask = cv2.medianBlur(whiteMask, 19)
         whiteMask = cv2.erode(whiteMask, None, iterations=2)
 	# whiteMask = cv2.dilate(whiteMask, None, iterations=2)
         #detecting the street
@@ -77,9 +76,13 @@ class robot_controller:
         redOutput = cv2.bitwise_and(warped_img, warped_img, mask = redMask)
         greyOutput = cv2.bitwise_and(warped_img, warped_img, mask = grayMask)
         whiteOutput = cv2.bitwise_and(warped_img, warped_img, mask = whiteMask)
+        imgray = cv2.cvtColor(whiteOutput, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+        _,contours,_  = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(whiteOutput, contours, -1, (255,0,0), 3)
         blueOutput = cv2.bitwise_and(warped_img, warped_img, mask = blueMask)
-        
-        edges = cv2.Canny(whiteOutput, 100, 200)
+        # print(contours)
+        edges = cv2.Canny(whiteOutput, 50, 150)
         lines = cv2.HoughLinesP(edges,1,np.pi/180, 20, 100)
         m = [] #list for slopes of the detected lines 
 
@@ -91,10 +94,27 @@ class robot_controller:
                 for points in line:
                     m.append(float (points[1] - points[3]) / (points[0] - points[2])) #  m = y/x
             m = [x for x in m if str(x) != 'nan'] #gets rid of nan values
+            # print(m)
+            # slopesWeighted = self.mergeSlopes(m)
+            # print("weighted slopes:")
+            # print(slopesWeighted)
+            longestLength = 0
+            longestLine = [0,0,0,0]
+            for i in range(len(lines)):
+                for x1,y1,x2,y2 in lines[i]:
+                    length = ((x2-x1)**2 + (y2-y1)**2)**(0.5)
+                    if(length > longestLength):
+                        longestLength = length
+                        longestLine = [x1,y1,x2,y2]
+                    # cv2.line(whiteOutput,(x1,y1), (x2,y2), (0,255,0), 2)
+            cv2.line(whiteOutput,(longestLine[0],longestLine[1]),(longestLine[2],longestLine[3]), (0,0,255), 2)
+            longestLineSlope = float((longestLine[1] - longestLine[3])/(longestLine[0] - longestLine[2]))
+            print("longest line slope:")
+            print(longestLineSlope)
+            # averageSlope = longestLineSlope
             sumOfSlopes = 0
             numOfSlopes = 0
             numberOfInfinity = 0
-            # print(m)
             for slope in m:
                 if slope == float("-inf") or slope == float("inf"):
                     numberOfInfinity = numberOfInfinity + 1
@@ -102,9 +122,9 @@ class robot_controller:
                     sumOfSlopes = sumOfSlopes + slope
                     numOfSlopes = numOfSlopes + 1
             # print("slopes")
-            # print (m)
-            # print ("sum of slopes")
-            # print (sumOfSlopes)
+            print (m)
+            print ("sum of slopes")
+            print (sumOfSlopes)
             print("num of slopes")
             print(numOfSlopes)
             print("num of inf slopes")
@@ -115,48 +135,9 @@ class robot_controller:
                 averageSlope = sumOfSlopes / numOfSlopes #we should turn
             print("average slope")
             print(averageSlope)
-            longestLength = 0
-            longestLine = [0,0,0,0]
+
             #drawing the hough lines on the white mask
             cv2.line(whiteOutput,(int(cols/2),int(rows/2)), (int(20 / averageSlope + cols/2), int(rows/2 + 20)),(255,0,0),2)
-            for i in range(len(lines)):
-                for x1,y1,x2,y2 in lines[i]:
-                    length = ((x2-x1)**2 + (y2-y1)**2)**(0.5)
-                    if(length > longestLength):
-                        longestLength = length
-                        longestLine = [x1,y1,x2,y2]
-                    # cv2.line(whiteOutput,(x1,y1), (x2,y2), (0,255,0), 2)
-            cv2.line(whiteOutput,(longestLine[0],longestLine[1]),(longestLine[2],longestLine[3]), (0,0,255), 2)
-            # cv2.imwrite("hough lines.jpg",warped_img)
-
-            # rowW,colW,rgbW = np.nonzero(whiteOutput)
-            # mostFrequentColumnW = np.argmax(np.bincount(colW))
-
-            # rowGY, colGY, rbgGY = np.nonzero(greyOutput)
-            # mostFrequentColumnGY = np.argmax(np.bincount(colGY))
-
-            # print("most frequent col:")
-            # print (mostFrequentColumnW)
-            # print("most frequent col grey:")
-            # print (mostFrequentColumnGY)
-
-            # targetLocation = 0
-            # lineOffset = 120
-            # if(mostFrequentColumnGY > mostFrequentColumnW):
-            #     targetLocation = mostFrequentColumnW + lineOffset
-            # else :
-            #     targetLocation = mostFrequentColumnW - lineOffset
-            # print("non zero columns")
-            # print(col)
-            # print ("hey")
-            # print(whiteOutput)
-            # print("non zero")
-            # print(np.nonzero(whiteOutput))
-
-
-            # cv2.circle(warped_img,(targetLocation,20),10,255)
-            # cv2.circle(warped_img,(mostFrequentColumnGY,20),10,0)
-            # cv2.circle(warped_img,(mostFrequentColumnW,20),10,120)
 
             cv2.imshow("whiteMask",whiteOutput)
             cv2.waitKey(3)
@@ -167,7 +148,7 @@ class robot_controller:
 
     def pid(self,slope):
         slopeThresh = 5000
-        angularScale = 2
+        angularScale = 5
         xVelocity = 0.03
         zTwist = 0.0
         if(abs(slope) < slopeThresh and abs(self.prevSlope) > slopeThresh):
@@ -181,6 +162,42 @@ class robot_controller:
         vel_msg.angular.z = zTwist
         # print (vel_msg)
         self.velocity_cmd.publish(vel_msg)
+    def mergeSlopes (self,slopes):
+        slopeTolerance = 1
+        weightedSlopes = {"slope": [], "number" : []}
+        weightedSlopes["slope"].append(float("inf"))
+        weightedSlopes["number"].append(0)
+        weightedSlopesCopy = weightedSlopes
+        # weightedSlopes["slope"].append(slopes.pop(0))
+        # weightedSlopes["number"].append(1)
+        for i, slope in enumerate(slopes):
+            print("slope in merge slope")
+            print(slope)
+            print("index")
+            print(i)
+            for index , diffSlope in enumerate(weightedSlopes["slope"]):
+                print("diff slope:")
+                print (diffSlope)
+                print("difference in slope")
+                print(abs(float(slope) - diffSlope))
+                print("index of the number")
+                print(index)
+                if (slope == float("inf") or slope == float("-inf")):
+                    print("infinity slope")
+                    weightedSlopes["number"][0] += 1
+                elif (abs(float(slope) - diffSlope) < slopeTolerance):
+                    weightedSlopes["number"][index] += 1
+                else:
+                    weightedSlopes["slope"].append(slope)
+                    weightedSlopes["number"].append(1)
+            print(weightedSlopes)
+        return weightedSlopes
+                    
+
+
+
+
+
         
 
 
