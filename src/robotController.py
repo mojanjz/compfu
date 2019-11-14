e561f0c4a2b4db7f0e06a1e56d66de9deebe287b
fizzer <fizzer@enph353.JoDo>
2019-11-13 18:24:07 -0800

    experimenting with intersection and gray mask

--- src/robotController.py
+++ src/robotController.py
@@ -37,24 +37,22 @@ class robot_controller:
         IMAGE_W = cols
 
         # #birdeye view of the image
-        # src = np.float32([[0, IMAGE_H], [1207, IMAGE_H], [0, 0], [IMAGE_W, 0]])
-        # dst = np.float32([[569, IMAGE_H], [711, IMAGE_H], [0, 0], [IMAGE_W, 0]])
-        # M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-        # Minv = cv2.getPerspectiveTransform(dst, src) # Inverse transformation
+        src = np.float32([[0, IMAGE_H], [1207, IMAGE_H], [0, 0], [IMAGE_W, 0]])
+        dst = np.float32([[569, IMAGE_H], [711, IMAGE_H], [0, 0], [IMAGE_W, 0]])
+        M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
+        Minv = cv2.getPerspectiveTransform(dst, src) # Inverse transformation
 
     
-        # cv_image = cv_image[450:(450+IMAGE_H), 0:IMAGE_W] # Apply np slicing for ROI crop
-        # warped_img = cv2.warpPerspective(cv_image, M, (IMAGE_W, IMAGE_H)) # Image warping
-        warped_img = cv_image[rows-200:, cols-400:cols] #CHANGE 
+        cv_warpedReady = cv_image[450:(450+IMAGE_H), 0:IMAGE_W] # Apply np slicing for ROI crop
+        warped_img = cv2.warpPerspective(cv_warpedReady, M, (IMAGE_W, IMAGE_H)) # Image warping
+        right_cropped = cv_image[rows-200:, cols-400:cols] #CHANGE 
         #color masks 
         #detecting lines on the street
         lowerWhite = np.array([250, 250, 250],dtype = "uint8")
         upperWhite = np.array([255, 255, 255],dtype = "uint8")
-        whiteMask = cv2.inRange(warped_img, lowerWhite, upperWhite)
-        # whiteMask = cv2.blur(whiteMask, (5,5))
+        whiteMask = cv2.inRange(right_cropped, lowerWhite, upperWhite)
         whiteMask = cv2.medianBlur(whiteMask, 5)
         whiteMask = cv2.erode(whiteMask, None, iterations=2)
-	# whiteMask = cv2.dilate(whiteMask, None, iterations=2)
         #detecting the street
         lowerGray = np.array([50, 80, 50],dtype = "uint8")
         upperGray = np.array([190, 90, 90],dtype = "uint8")
@@ -62,21 +60,21 @@ class robot_controller:
         #grass green
         lowerGreen = np.array([10,70,10],dtype = "uint8")
         upperGreen = np.array([70,210,30],dtype = "uint8")
-        greenMask = cv2.inRange(warped_img, lowerGreen, upperGreen)
+        greenMask = cv2.inRange(right_cropped, lowerGreen, upperGreen)
         #red for cross walk
         lowerRed = np.array([0, 0, 255-20],dtype = "uint8")
         upperRed = np.array([255, 20, 255],dtype = "uint8")
-        redMask = cv2.inRange(warped_img, lowerRed, upperRed)
+        redMask = cv2.inRange(right_cropped, lowerRed, upperRed)
         #blue for car detection
         lowerBlue = np.array([0, 0, 0],dtype = "uint8")
         upperBlue = np.array([255, 30, 20],dtype = "uint8")
-        blueMask = cv2.inRange(warped_img, lowerBlue, upperBlue)
+        blueMask = cv2.inRange(right_cropped, lowerBlue, upperBlue)
         #apply masks for lane detection
-        greenOutput = cv2.bitwise_and(warped_img, warped_img, mask = greenMask)
-        redOutput = cv2.bitwise_and(warped_img, warped_img, mask = redMask)
-        grayOutput = cv2.bitwise_and(warped_img, warped_img, mask = grayMask)
-        whiteOutput = cv2.bitwise_and(warped_img, warped_img, mask = whiteMask)
-        blueOutput = cv2.bitwise_and(warped_img, warped_img, mask = blueMask)
+        greenOutput = cv2.bitwise_and(right_cropped, right_cropped, mask = greenMask)
+        redOutput = cv2.bitwise_and(right_cropped, right_cropped, mask = redMask)
+        grayOutput = cv2.bitwise_and(warped_img, warped_img, mask = grayMask) #masking the cv_image
+        whiteOutput = cv2.bitwise_and(right_cropped, right_cropped, mask = whiteMask)
+        blueOutput = cv2.bitwise_and(right_cropped, right_cropped, mask = blueMask)
 
         grayWarped = cv2.cvtColor(whiteOutput,cv2.COLOR_BGR2GRAY)
         ret,thresh = cv2.threshold(grayWarped, 20, 255, 0)
@@ -94,15 +92,22 @@ class robot_controller:
             cY = rows - 200 + int(M["m01"]/ M["m00"])
             middlePixel = cols/2
             offset = cX - middlePixel
-            print("current offset:")
-            print(offset)
-            print("target offset: ")
-            print(self.targetOffset)
+            # print("current offset:")
+            # print(offset)
+            # print("target offset: ")
+            # print(self.targetOffset)
+        print("grayOutput")
+        print(grayOutput)
+        print("cv_image")
+        print(cv_image)
+        grayPercentage =float( np.count_nonzero(np.asarray(grayOutput))) / float(np.count_nonzero(np.asarray(warped_img)))
+        print(grayPercentage)
+        # print(np.count_nonzero(np.asarray(warped_img)))
         cv2.circle(cv_image,(middlePixel,cY), 5, (255,0,0))
         cv2.circle(cv_image, (cX,cY), 5, (255,0,0))
-        cv2.imshow("whiteMask",whiteOutput)
+        cv2.imshow("grayMask",grayOutput)
         cv2.waitKey(3)
-        cv2.imshow("Image window", warped_img)
+        cv2.imshow("Image window", right_cropped)
         cv2.waitKey(3)
         cv2.imshow("contour image",cv_image)
         cv2.waitKey(3)
@@ -110,26 +115,24 @@ class robot_controller:
         self.pid(offset)
 
     def pid(self,offset):
-        # slopeThresh = 5000
         differenceTolerance = 55
         angularScale = 5
         xVelocity = 0.03
         zTwist = 0.0
         offsetOvershoot = self.targetOffset - offset
-        print("offset overshoot")
-        print(offsetOvershoot )
+        # print("offset overshoot")
+        # print(offsetOvershoot )
         if(abs(offsetOvershoot) > differenceTolerance):
-            print("turning")
-            if(offsetOvershoot > 0):
-                print ("right")
-            else:
-                print("left")
+            # print("turning")
+            # if(offsetOvershoot > 0):
+            #     print ("right")
+            # else:
+            #     print("left")
             xVelocity = 0.0
             zTwist = angularScale * offsetOvershoot
         vel_msg = Twist()
         vel_msg.linear.x = xVelocity
         vel_msg.angular.z = zTwist
-        # print (vel_msg)
         self.velocity_cmd.publish(vel_msg)
         
 
