22deceb365aef753fd5760bd65427ab87ec79e2a
fizzer <fizzer@enph353.JoDo>
2019-11-18 22:17:04 -0800

    added some if statements to detect crosswalks, not functional yet

--- src/robotController.py
+++ src/robotController.py
@@ -25,6 +25,9 @@ class robot_controller:
         self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
         self.velocity_cmd = rospy.Publisher('/R1/cmd_vel', Twist,queue_size=1)
         self.targetOffset = 450
+        self.sawCrosswalk = False
+        self.atCrosswalk = False
+        self.offCrosswalk = True
 
     def callback(self,data):
         try:
@@ -46,6 +49,7 @@ class robot_controller:
         # cv_image = cv_image[450:(450+IMAGE_H), 0:IMAGE_W] # Apply np slicing for ROI crop
         # warped_img = cv2.warpPerspective(cv_image, M, (IMAGE_W, IMAGE_H)) # Image warping
         warped_img = cv_image[rows-200:, cols-400:cols] #CHANGE 
+        crosswalkImage = cv_image[rows-200:,0:cols]
         #color masks 
         #detecting lines on the street
         lowerWhite = np.array([250, 250, 250],dtype = "uint8")
@@ -66,30 +70,54 @@ class robot_controller:
         #red for cross walk
         lowerRed = np.array([0, 0, 255-20],dtype = "uint8")
         upperRed = np.array([255, 20, 255],dtype = "uint8")
-        redMask = cv2.inRange(warped_img, lowerRed, upperRed)
+        redMask = cv2.inRange(crosswalkImage, lowerRed, upperRed)
         #blue for car detection
         lowerBlue = np.array([0, 0, 0],dtype = "uint8")
         upperBlue = np.array([255, 30, 20],dtype = "uint8")
         blueMask = cv2.inRange(warped_img, lowerBlue, upperBlue)
         #apply masks for lane detection
         greenOutput = cv2.bitwise_and(warped_img, warped_img, mask = greenMask)
-        redOutput = cv2.bitwise_and(warped_img, warped_img, mask = redMask)
+        redOutput = cv2.bitwise_and(crosswalkImage, crosswalkImage, mask = redMask)
         grayOutput = cv2.bitwise_and(warped_img, warped_img, mask = grayMask)
         whiteOutput = cv2.bitwise_and(warped_img, warped_img, mask = whiteMask)
         blueOutput = cv2.bitwise_and(warped_img, warped_img, mask = blueMask)
 
+        offset = 0
+        redPercentage =float( np.count_nonzero(np.asarray(redOutput))) / float(np.count_nonzero(np.asarray(crosswalkImage)))
+        if(redPercentage > 0.0 and self.offCrosswalk == True):
+            print("saw a crosswalk")
+            self.sawCrosswalk = True
+            self.offCrosswalk = False
+        elif(self.sawCrosswalk == True and redPercentage == 0.0):
+            print("on crosswalk")
+            self.atCrosswalk = True
+            self.offCrosswalk = False
+        elif(self.atCrosswalk == True and redPercentage > 0.0):
+            print("going off cross walk")
+        elif(self.atCrosswalk == True and redPercentage == 0.0):
+            print("passed the crosswalk")
+            self.offCrosswalk == True
+        if(self.offCrosswalk == True):
+            print("off the cross walk")
+            self.sawCrosswalk = False
+            self.atCrosswalk = False
+
+        
+            
+            
         
         grayWarped = cv2.cvtColor(whiteOutput,cv2.COLOR_BGR2GRAY)
         ret,thresh = cv2.threshold(grayWarped, 20, 255, 0)
         img, contours, hierarchy = cv2.findContours(thresh, 1, 2)
         #find center of mass
         M = cv2.moments(img)
-        offset = 0
         middlePixel = 0
         cX = 0
         cY = 0
         if(M["m00"] == 0):
             offset = self.targetOffset
+        # elif(self.atCrosswalk == True):
+        #     offset = self.targetOffset
         else:
             cX = cols - 400 + int(M["m10"]/M["m00"])
             cY = rows - 200 + int(M["m01"]/ M["m00"])
@@ -103,7 +131,8 @@ class robot_controller:
         cv2.circle(cv_image, (cX,cY), 5, (255,0,0))
         # cv2.imshow("whiteMask",whiteOutput)
         # cv2.waitKey(3)
-        cv2.imshow("Image window", warped_img)
+
+        cv2.imshow("Image window", redOutput)
         cv2.waitKey(3)
         cv2.imshow("contour image",cv_image)
         cv2.waitKey(3)
